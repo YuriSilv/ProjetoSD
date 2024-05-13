7dac97331dfb6f6792654359a6b5114b9399b16f
YuriSilv <yuri.silvino@ufvjm.edu.br>
2024-05-13 17:35:41 -0300

    api funcional

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.get_data_pesquisador, name='get_data_pesquisador'),
     path('add/', views.add_data_pesquisador, name='add_data_pesquisador'),
-    path('update/', views.add_data_pesquisador, name='update_data_pesquisador'),
-    path('delete/', views.add_data_pesquisador, name='delete_data_pesquisador'),
+    path('update/<int:id>/', views.update_data_pesquisador, name='update_data_pesquisador'),
+    path('delete/<int:id>/', views.delete_data_pesquisador, name='delete_data_pesquisador'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,25 +1,22 @@
-from rest_framework.response import Response #pega o dado e transforma em json
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from equipe.models import Pesquisador
 from .serializers import PesquisadorSerializer
 from rest_framework import status
 
-
 @api_view(['GET'])
 def get_data_pesquisador(request):
     pesquisador = Pesquisador.objects.all()
     serializer = PesquisadorSerializer(pesquisador, many=True)
-    return Response(serializer.data) #recebe a info em dict e retorna o json
-
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def add_data_pesquisador(request):
     serializer = PesquisadorSerializer(data=request.data)
-
     if serializer.is_valid():
         serializer.save()
-
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 def delete_data_pesquisador(request, id):
