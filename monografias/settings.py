d69721c6b303c07a2716c66afe811e4a06dc5144
YuriSilv <yuri.silvino@ufvjm.edu.br>
2024-05-22 19:38:07 -0300

    recuperação de senha

--- monografias/settings.py
+++ monografias/settings.py
@@ -143,3 +143,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Media files (User uploaded content)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'email@gmail'
+EMAIL_HOST_PASSWORD = 'senha'
